7a901cd572cb43744c792567381e206971ad7b14
Vladimir <zukrac@yandex.ru>
2026-01-10 15:21:50 +0300

    Moved to separate folder

--- restapi1-src/app/Http/Controllers/Api/ApiController.php
+++ restapi1-src/app/Http/Controllers/Api/ApiController.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Api;
 
+use App\Http\Controllers\Controller;
 use App\Http\Requests\CompanySearchRequest;
 use App\Http\Resources\CompanyResource;
 use App\Models\Company;
--- restapi1-src/routes/api.php
+++ restapi1-src/routes/api.php
@@ -3,9 +3,9 @@
 use App\Http\Middleware\EnsureTokenIsValid;
 use Illuminate\Support\Facades\Route;
 
-Route::controller(\App\Http\Controllers\ApiController::class)
+Route::controller(\App\Http\Controllers\Api\ApiController::class)
     ->middleware([EnsureTokenIsValid::class])
     ->group(function () {
         Route::get('/get', 'get');
         Route::post('/search', 'search');
-});
+    });
